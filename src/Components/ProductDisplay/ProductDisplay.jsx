08527599bd8550465f92ab4c25b02486a586c62e
afran18 <afran.vk18@gmail.com>
2024-09-04 20:04:09 +0530

    Login sign up and product display page

---
+++ src/Components/ProductDisplay/ProductDisplay.jsx
@@ -0,0 +1,60 @@
+import React from 'react'
+import './ProductDisplay.css'
+import star_icon from '../Assets/star_icon.png'
+import star_dull_icon from '../Assets/star_dull_icon.png'
+
+const ProductDisplay = (props) => {
+    const {product} = props;
+  return (
+    <div className='productdisplay'>
+        <div className="productdisplay-left">
+            <div className="productdisplay-img-list">
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+                <img src={product.image} alt="" />
+            </div>
+            <div className="productdisplay-img">
+                <img className = "productdisplay-main-img" src={product.image} alt="" />
+            </div>
+        </div>
+        <div className="productdisplay-right">
+            <h1>{product.name}</h1>
+            <div className="productdisplay-right-stars">
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_icon} alt="" />
+                <img src={star_dull_icon} alt="" />
+                <p>(122)</p>
+            </div>
+            <div className="productdisplay-right-prices">
+                <div className="productdisplay-right-price-old">${product.old_price}</div>
+                <div className="productdisplay-right-price-new">${product.new_price}</div>
+            </div>
+            <div className="productdisplay-right-description">
+            This beige polo t-shirt by WES Casuals features an evergreen striped pattern in a relaxed fit. Crafted from lightweight fabric for added breathability, this classic old-school design is finished with a cutaway collar and short sleeves, making it a timeless choice for casual wear.
+            </div>
+            <div className="productdisplay-right-size">
+                <h1>Select size</h1>
+                <div className="productdisplay-right-sizes">
+                    <div className='selected-size'>S</div>
+                    <div className='selected-size'>M</div>
+                    <div className='selected-size'>L</div>
+                    <div className='selected-size'>XL</div>
+                    <div className='selected-size'>XXL</div>
+                </div>
+            </div>
+            <button>ADD TO CART</button>
+            <p className="productdisplay-right-category">
+                <span>Category: </span> Women, T Shirt, Slim Fit
+            </p>
+            <p className="productdisplay-right-category">
+                <span>Tags: </span> Modern, Latest, Trending
+            </p>
+        </div>
+    </div>
+  )
+}
+
+export default ProductDisplay
--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
   return (
-    <div>LoginSignup</div>
+    <div class loginsignup>
+      <div className="loginsignup-container">
+        <h1>Sign up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder='Your name'/>
+          <input type="email" placeholder='Your email'/>
+          <input type="password" placeholder='Create Password'/>
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">Already have an account? <span>Login</span></p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>By continuing I agree to accept the privacy policy.</p>
+        </div>
+      </div>
+    </div>
   )
 }
 
